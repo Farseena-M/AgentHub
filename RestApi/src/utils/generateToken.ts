b294a40c7e069cab43853f58e96f6efeb9b231b7
Farseena-M <farseenamoyan19@gmail.com>
2025-01-29 22:42:37 +0530

    updated

--- RestApi/src/utils/generateToken.ts
+++ RestApi/src/utils/generateToken.ts
@@ -3,6 +3,6 @@ import jwt from 'jsonwebtoken';
 export const generateToken = (id: string) => {
     return jwt.sign(
         { id }, `${process.env.JWT_SECRET}`, {
-        expiresIn: process.env.LOGIN_EXPIRES
+        expiresIn: 60 * 60 * 24
     })
 }
