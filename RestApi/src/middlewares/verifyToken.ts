f06d2c57cc910f2426b0ec31d801eebded874254
Farseena-M <farseenamoyan19@gmail.com>
2025-01-29 23:49:58 +0530

    updated

---
+++ RestApi/src/middlewares/verifyToken.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { IUser } from '../interface/userInterface';
+import { User } from '../models/userSchema';
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: IUser;
+        }
+    }
+}
+
+export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(403).json({ message: 'Access denied' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, `${process.env.JWT_SECRET}`) as JwtPayload;
+
+        const userId = decoded.id;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid token' });
+    }
+};
